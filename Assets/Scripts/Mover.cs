e9b2ac832dc6bc032b96ec67b07bf8c5600ede86
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-04 05:18:03 +0100

    Update Mover.cs
    Added instructions via Debug.Log.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,14 +10,27 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
         float zValue = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
         transform.Translate(xValue, 0f, zValue);
     }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome!");
+        Debug.Log("Move the player with the WSAD or the arrow keys.");
+        Debug.Log("Do not hit any obstacles!");
+    }
+
 }
